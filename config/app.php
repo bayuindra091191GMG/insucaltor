d8a0b88456e560c04b54ff313ca6f13c78636a15
GMG-Executive <randf77@gmail.com>
2017-08-18 16:15:34 +0700

    Change App Name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Example API',
+    'name' => 'Insurance Calculator',
 
     /*
     |--------------------------------------------------------------------------
