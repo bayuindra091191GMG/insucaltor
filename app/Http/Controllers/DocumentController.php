a2ede7c21a3710bd02bab375a9f6ace332d4896e
ferdyantorand <randf77@gmail.com>
2017-10-25 16:02:35 +0700

    Split Function to Handle Otomate Documents Add Fucntion to Send Comprehensive Document

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -29,6 +29,8 @@ class DocumentController extends Controller
           "rate" : "4.80"
         }
     }*/
+
+    // Send Otomate & Otomate Smart
     public function SendDocument(){
         try {
             $json_result = file_get_contents('php://input');
@@ -36,17 +38,136 @@ class DocumentController extends Controller
             //$harga_kendaraan = number_format($json->harga_kendaraan, 0, ",", ".");
             $newFileName = $json->agent_name.Carbon::now('Asia/Jakarta')->format('Ymdhms');
 
+            if($json->type == 'otomate'){
+                $filePath = '/Surat Penawaran New otomate.xlsx';
+            }
+            else{
+                $filePath = '/Surat Penawaran New otomate - Smart.xlsx';
+            }
+
             $path = public_path('document/');
-            Excel::load($path . '/Surat Penawaran New otomate.xlsx', function($reader) use($json)
+            Excel::load($path . $filePath, function($reader) use($json)
             {
                 $reader->sheet('Otomate', function($sheet) use($json)
                 {
                     //Set The field Data
+                    $sheet->getCell('A10')->setValueExplicit($json->nama_tertanggung);
                     $sheet->getCell('H9')->setValueExplicit('Tangerang, '.Carbon::now('Asia/Jakarta')->format('d M Y'));
                     $sheet->getCell('E21')->setValueExplicit($json->nama_tertanggung);
-                    $sheet->getCell('E23')->setValueExplicit($json->merek_kendaraan);
+                    $sheet->getCell('E23')->setValueExplicit($json->jenis_kendaraan);
                     $sheet->getCell('E25')->setValueExplicit($json->tahun_kendaraan);
                     $sheet->getCell('E29')->setValueExplicit('Rp'.number_format($json->nilai_pertanggungan, 0, ",", "."));
+                    $sheet->getCell('F33')->setValueExplicit($json->rate.'%');
+                    $sheet->getCell('G33')->setValueExplicit('Rp'.number_format($json->nilai_pertanggungan, 0, ",", "."));
+                    $sheet->getCell('H33')->setValueExplicit('Rp'.number_format($json->premi, 0, ",", "."));
+                    $sheet->getCell('G40')->setValueExplicit($json->third_party);
+                    $sheet->getCell('G41')->setValueExplicit($json->personal_accident);
+                    $sheet->getCell('H43')->setValueExplicit('Rp'.number_format($json->premi, 0, ",", "."));
+                    $sheet->getCell('H45')->setValueExplicit('Rp'.number_format($json->premi, 0, ",", "."));
+                    $sheet->getCell('A63')->setValueExplicit($json->agent_name);
+                });
+            })
+                ->setFilename($newFileName)
+                ->store('xls', public_path('user_docs/'));
+
+            //Send the Email
+            Mail::send('emails.offer', ['user' => '123'], function ($message) use($newFileName, $json){
+                //
+                $message->attach(public_path('user_docs/'.$newFileName.'.xls'));
+                $message->from('randf77@gmail.com', 'Offering Letter By ICalculator');
+                $message->to($json->user_email, $json->nama_tertanggung)->subject('Offering Letter!');
+            });
+
+            return response('Success Send Surat Penaawaran!', 200)
+                ->header('Content-Type', 'text/plain');
+        }
+        catch (Exception $ex){
+            //Utilities::ExceptionLog($ex);
+            return response($ex, 200)
+                ->header('Content-Type', 'text/plain');
+        }
+    }
+
+    // Function to send Otomate Solitare Document
+    public function SendSolitareDocument(){
+        try {
+            $json_result = file_get_contents('php://input');
+            $json = json_decode($json_result);
+            //$harga_kendaraan = number_format($json->harga_kendaraan, 0, ",", ".");
+            $newFileName = $json->agent_name.Carbon::now('Asia/Jakarta')->format('Ymdhms');
+
+            $path = public_path('document/');
+            Excel::load($path . '/Surat Penawaran New otomate - Solitare.xlsx', function($reader) use($json)
+            {
+                $reader->sheet('Otomate', function($sheet) use($json)
+                {
+                    //Set The field Data
+                    $sheet->getCell('A10')->setValueExplicit($json->nama_tertanggung);
+                    $sheet->getCell('H9')->setValueExplicit('Tangerang, '.Carbon::now('Asia/Jakarta')->format('d M Y'));
+                    $sheet->getCell('E21')->setValueExplicit($json->nama_tertanggung);
+                    $sheet->getCell('E23')->setValueExplicit($json->jenis_kendaraan);
+                    $sheet->getCell('E25')->setValueExplicit($json->tahun_kendaraan);
+                    $sheet->getCell('E29')->setValueExplicit('Rp'.number_format($json->nilai_pertanggungan, 0, ",", "."));
+                    $sheet->getCell('G33')->setValueExplicit('Rp'.number_format($json->nilai_pertanggungan, 0, ",", "."));
+                    $sheet->getCell('H33')->setValueExplicit('Rp'.number_format($json->premi, 0, ",", "."));
+                    $sheet->getCell('G41')->setValueExplicit($json->third_party);
+                    $sheet->getCell('F33')->setValueExplicit($json->rate.'%');
+                    $sheet->getCell('G42')->setValueExplicit($json->personal_accident);
+                    $sheet->getCell('H44')->setValueExplicit('Rp'.number_format($json->premi, 0, ",", "."));
+                    $sheet->getCell('H46')->setValueExplicit('Rp'.number_format($json->premi, 0, ",", "."));
+                    $sheet->getCell('A64')->setValueExplicit($json->agent_name);
+                });
+            })
+                ->setFilename($newFileName)
+                ->store('xls', public_path('user_docs/'));
+
+            //Send the Email
+            Mail::send('emails.offer', ['user' => '123'], function ($message) use($newFileName, $json){
+                //
+                $message->attach(public_path('user_docs/'.$newFileName.'.xls'));
+                $message->from('randf77@gmail.com', 'Offering Letter By ICalculator');
+                $message->to($json->user_email, $json->nama_tertanggung)->subject('Offering Letter!');
+            });
+
+            return response('Success Send Surat Penaawaran!', 200)
+                ->header('Content-Type', 'text/plain');
+        }
+        catch (Exception $ex){
+            //Utilities::ExceptionLog($ex);
+            return response($ex, 200)
+                ->header('Content-Type', 'text/plain');
+        }
+    }
+
+    // Function to Send Comprehensive Document
+    public function SendComprehensiveDocument(){
+        try {
+            $json_result = file_get_contents('php://input');
+            $json = json_decode($json_result);
+            //$harga_kendaraan = number_format($json->harga_kendaraan, 0, ",", ".");
+            $newFileName = $json->agent_name.Carbon::now('Asia/Jakarta')->format('Ymdhms');
+
+            if($json->type == 'otomate'){
+                $filePath = '/Surat Penawaran New otomate.xlsx';
+            }
+            else{
+                $filePath = '/Surat Penawaran New otomate - Smart.xlsx';
+            }
+
+            $path = public_path('document/');
+            Excel::load($path . $filePath, function($reader) use($json)
+            {
+                $reader->sheet('Otomate', function($sheet) use($json)
+                {
+                    //Set The field Data
+                    $sheet->getCell('A10')->setValueExplicit($json->nama_tertanggung);
+                    $sheet->getCell('H9')->setValueExplicit('Tangerang, '.Carbon::now('Asia/Jakarta')->format('d M Y'));
+                    $sheet->getCell('E21')->setValueExplicit($json->nama_tertanggung);
+                    $sheet->getCell('E23')->setValueExplicit($json->jenis_kendaraan);
+                    $sheet->getCell('E25')->setValueExplicit($json->tahun_kendaraan);
+                    $sheet->getCell('E29')->setValueExplicit('Rp'.number_format($json->nilai_pertanggungan, 0, ",", "."));
+
+                    // Table Field
                     $sheet->getCell('G33')->setValueExplicit('Rp'.number_format($json->nilai_pertanggungan, 0, ",", "."));
                     $sheet->getCell('H33')->setValueExplicit('Rp'.number_format($json->premi, 0, ",", "."));
                     $sheet->getCell('G40')->setValueExplicit($json->third_party);
